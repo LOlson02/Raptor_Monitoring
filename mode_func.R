70d84b5fff6cc0e89b1da457a34b434fa21354f6
Lucretia Olson <lucretia.o@gmail.com>
2019-12-16 11:40:41 -0700

    changing functions to sources

---
+++ mode_func.R
@@ -0,0 +1,7 @@
+#function for mode
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+
--- spatial_monitoring_simulation_6Dec2019.R
+++ spatial_monitoring_simulation_6Dec2019.R
@@ -7,6 +7,8 @@ library(spatialEco)
 
 
 options(scipen=999)
+source('E:/Lucretia/Ferruginous_Hawk_Project/Monitoring/Raptor_Monitoring/mode_func.R')
+source('E:/Lucretia/Ferruginous_Hawk_Project/Monitoring/Raptor_Monitoring/hex_grid_func.R')
 
 #setwd
 setwd("E:/Lucretia/Ferruginous_Hawk_Project/Monitoring")
@@ -138,41 +140,6 @@ hist.hawks$bin <- extract(hawk.rsf,hist.hawks)
 table(hist.hawks$bin)/nrow(hist.hawks)
 rand.nests$bin <- extract(hawk.rsf,rand.nests)
 table(rand.nests$bin)/nrow(rand.nests)
-
-
-#function to make hexagonal grids http://strimas.com/spatial/hexagonal-grids/
-
-make_grid <- function(x, cell_diameter, cell_area, clip = FALSE) {
-  if (missing(cell_diameter)) {
-    if (missing(cell_area)) {
-      stop("Must provide cell_diameter or cell_area")
-    } else {
-      cell_diameter <- sqrt(2 * cell_area / sqrt(3))
-    }
-  }
-  ext <- as(extent(x) + cell_diameter, "SpatialPolygons")
-  projection(ext) <- projection(x)
-  # generate array of hexagon centers
-  g <- spsample(ext, type = "hexagonal", cellsize = cell_diameter, 
-                offset = c(0.5, 0.5))
-  # convert center points to hexagons
-  g <- HexPoints2SpatialPolygons(g, dx = cell_diameter)
-  # clip to boundary of study area
-  if (clip) {
-    g <- gIntersection(g, x, byid = TRUE)
-  } else {
-    g <- g[x, ]
-  }
-  # clean up feature IDs
-  row.names(g) <- as.character(1:length(g))
-  return(g)
-}
-
-#function for mode
-getmode <- function(v) {
-   uniqv <- unique(v)
-   uniqv[which.max(tabulate(match(v, uniqv)))]
-}
 
 
 
